c1bb243df95ed677c18d89f113326f0592f27f22
tokaji <fpzk5656@gmail.com>
2021-04-05 05:58:54 +0900

    range-based for loop 방식. vector.size 사용할 때 주의해야될 방식

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -1,54 +1,21 @@
 #include <iostream>
 using namespace std;
-
-//배열에서 선택한 인덱스에 원소가 추가되고, 삭제되는 함수 만들기
-void insert(int idx, int num, int arr[], int& len)
-{
-  for(int i = len; i > idx; i--)
-  {
-    arr[i] = arr[i - 1];
-  }
-  arr[idx] = num;
-  len++;
-
-  for(int i = 0; i < len; i++)
-  {
-    if(i == len-1)
-    {
-      cout << arr[i]<< "\n\n";
-    }
-    else
-    {
-    cout << arr[i] << "\n";
-    }
-  }
-}
-void erase(int idx, int arr[], int& len)
-{
- len--;
-  for(int i = idx; i < len; i++)
-  {
-    arr[i] = arr[i+1];
-  }
-
-  for(int i = 0; i < len; i++)
-  {
-    if(i == len-1)
-    {
-    cout << arr[i] << "\n\n";
-    }
-    else
-    {
-      cout << arr[i] << "\n";
-    }
-  }
-}
+#include <vector>
 
 int main()
 {
-    int arr[10] = {10,50,40,30,70,20};
-    int len = 6;
-    insert(3, 60, arr, len); // 10 50 40 60 30 70 20
-    erase(4, arr, len); // 10 50 40 60 70 20
-    return 0;
+  vector<int> v1 = {1,2,3,4,5,6};
+  // 1. range-based for loop (single C++11)
+  for(int e : v1)
+  cout << e << ' ';
+
+  // 2. not bad
+  for(int i = 0; i < v1.size(); i++)
+  cout << v1[i] << ' ';
+
+  // 3. *** WORNG ***
+  for(int i = 0; i <= v1.size()-1; i++)
+  cout << v1[i] << ' ';
+
+  return 0;
 }
