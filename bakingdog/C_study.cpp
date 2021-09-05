1c8747df14af4a0b5a30d47ed7090d61c02b41fa
tokaji <fpzk5656@gmail.com>
2021-09-05 18:58:00 +0900

    0x0강 문제 리펙터링

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -24,10 +24,58 @@ int func1(int N)
   return result;
 }
 
+// int func2(int arr[], int N)
+// {
+//   for(int i = 0; i < N; i++)
+//   {
+//     // 0부터 N까지 한번씩 더해보는 것
+//     for(int j = i+1; j < N; j++)
+//     {
+//         if(arr[i] + arr[j] == 100)
+//         return 1;
+//     }
+//   }
+
+//   return 0;
+// }
+
+// 좀 더 빠른 시간복잡도
+int func2(int arr[], int N)
+{
+  int record[101];
+
+  for(int i = 0; i < 101; i++)
+  {
+    record[i] = 0;
+  }
+
+  for(int i = 0; i < N; i++)
+  {
+    //arr[0] 이 32고
+    //arr[2] 이 68 이라 치면
+    int compare = 100 - arr[i];
+    if(record[compare] == 1)
+    {
+      return 1;
+    }
+    record[arr[i]] = 1;
+  }
+
+  return 0;
+}
+
 int main()
 {
- cout << func1(16) << endl;
- cout << func1(34567);
+  cout << func1(16) << endl;
+  cout << func1(34567) << endl;
 
+
+  int a[3] = {1,52,48};
+  int b[2] = {50,42};
+  int c[4] = {4,13,63,87};
+
+  cout << func2(a,3) << endl;
+  cout << func2(b,2) << endl;
+  cout << func2(c,4) << endl;
   return 0;
 }
