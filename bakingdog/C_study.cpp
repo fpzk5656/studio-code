eb3260a92722c9c505a75265b9665f70a309f5c3
tokaji <fpzk5656@gmail.com>
2021-09-15 22:33:23 +0900

    바킹독 0X3강 배열 원소 추가 및 삭제 함수 구현

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -3,109 +3,45 @@ using namespace std;
 #include <vector>
 #include <algorithm>
 
-//다시 복귀
+// 바킹독 0x3강
 
-// 바킹독 0x0강
-// 문제 1
-// N 이하의 자연수 중에서 3의 배수이거나, 5의 배수인 수를 모두 합한 값을
-// 반환하는 함수 func1(int N)을 작성하라. N은 5만 이하의 자연수이다.
-
-// 시간복잡도 O(N^2)
-int func1(int N)
+// 배열 임의의 원소 추가
+void insert(int idx, int num, int arr[], int& len)
 {
-  int length = N;
-  int result = 0;
-  for(int i = 0; i < length; i++)
+  for(int i = len; i > idx; i--)
   {
-    if(i % 3 == 0 || i % 5 == 0)
-    {
-        result += i;
-    }
+    arr[i] = arr[i-1];
   }
-  return result;
+  arr[idx] = num;
+  len++;
+
+  for(int i = 0; i < len; i++)
+  {
+    cout << arr[i] << " ";
+  }
 }
 
-// 시간복잡도 O(N)
-// int func2(int arr[], int N)
-// {
-//   for(int i = 0; i < N; i++)
-//   {
-//     // 0부터 N까지 한번씩 더해보는 것
-//     for(int j = i+1; j < N; j++)
-//     {
-//         if(arr[i] + arr[j] == 100)
-//         return 1;
-//     }
-//   }
-
-//   return 0;
-// }
-
-// 좀 더 빠른 시간복잡도
-int func2(int arr[], int N)
+// 배열 임의의 원소 삭제
+void erase(int idx, int arr[], int& len)
 {
-  int record[101];
-
-  for(int i = 0; i < 101; i++)
+  for(int i = idx; i < len; i++)
   {
-    record[i] = 0;
+      arr[i] = arr[i+1];
   }
+  len--;
 
-  for(int i = 0; i < N; i++)
+  for(int i = 0; i < len; i++)
   {
-    //arr[0] 이 32고
-    //arr[2] 이 68 이라 치면
-    int compare = 100 - arr[i];
-    if(record[compare] == 1)
-    {
-      return 1;
-    }
-    record[arr[i]] = 1;
+    cout << arr[i] << " ";
   }
-
-  return 0;
-}
-
-int func3(int N)
-{
-  for(int i = 1; i <= N; i++)
-  {
-    if(i * i == N) return 1;
-  }
-  return 0;
-}
-
-int func4(int N)
-{
-  int num = 2;
-  while(num * 2 <= N)
-  {
-    num *= 2;
-  }
-  
-    return num;
 }
 
 int main()
 {
-  cout << func1(16) << endl;
-  cout << func1(34567) << endl;
+  int arr[10] = {10, 50, 40, 30, 70, 20};
+  int len = 6;
+  insert(3, 60, arr, len);
+  erase(4, arr, len);
 
-
-  int a[3] = {1,52,48};
-  int b[2] = {50,42};
-  int c[4] = {4,13,63,87};
-
-  cout << func2(a,3) << endl;
-  cout << func2(b,2) << endl;
-  cout << func2(c,4) << endl;
-  cout << "====" << endl;
-  cout << func3(9) << endl;
-  cout << func3(693953651) << endl;
-  cout << func3(756580036) << endl;
-  cout << "====" << endl;
-  cout << func4(5) <<endl;
-  cout << func4(97615282) << endl;
-  cout << func4(1024) << endl;
   return 0;
 }
