e0b660fb64d750b172d690635fdefae3473f970a
tokaji <fpzk5656@gmail.com>
2021-09-17 02:45:18 +0900

    전체를 특정값으로 초기화 하는 방법

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 // 바킹독 0x3강
 
@@ -19,6 +20,7 @@ void insert(int idx, int num, int arr[], int& len)
   {
     cout << arr[i] << " ";
   }
+  cout << "\n";
 }
 
 // 배열 임의의 원소 삭제
@@ -34,7 +36,11 @@ void erase(int idx, int arr[], int& len)
   {
     cout << arr[i] << " ";
   }
+  cout << "\n";
 }
+
+
+
 
 int main()
 {
@@ -42,6 +48,28 @@ int main()
   int len = 6;
   insert(3, 60, arr, len);
   erase(4, arr, len);
+
+  // 전체를 특정값으로 초기화 하는 방법
+int a[21];
+int b[21][21];
+
+// 1.memset (비추천) cstring 헤더
+memset(a, 0, sizeof a);
+memset(b, 0, sizeof b);
+
+// 2.for (추천)
+for(int i = 0; i < 21; i++)
+	a[i] = 0;
+for(int i = 0; i < 21; i++)
+	for(int j = 0; j < 21; j++)
+    	b[i][j] = 0;
+        
+// 3.fill (추천)
+fill(a, a+21, 0);
+for(int i = 0; i < 21; i++)
+	fill(b[i], b[i]+21, 0);
+
+
 
   return 0;
 }
