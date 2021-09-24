40ab26870d30af2c5e54ee913c0344d8d90098b1
tokaji <fpzk5656@gmail.com>
2021-09-25 04:18:48 +0900

    벡터 사용법

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,69 +6,24 @@ using namespace std;
 
 // 바킹독 0x3강
 
-// 배열 임의의 원소 추가
-void insert(int idx, int num, int arr[], int& len)
-{
-  for(int i = len; i > idx; i--)
-  {
-    arr[i] = arr[i-1];
-  }
-  arr[idx] = num;
-  len++;
-
-  for(int i = 0; i < len; i++)
-  {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-}
-
-// 배열 임의의 원소 삭제
-void erase(int idx, int arr[], int& len)
-{
-  for(int i = idx; i < len; i++)
-  {
-      arr[i] = arr[i+1];
-  }
-  len--;
-
-  for(int i = 0; i < len; i++)
-  {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-}
-
-
-
-
+//Vector
 int main()
 {
-  int arr[10] = {10, 50, 40, 30, 70, 20};
-  int len = 6;
-  insert(3, 60, arr, len);
-  erase(4, arr, len);
+  vector<int> v1(3, 5); //{5, 5, 5};
+  cout << v1.size() << '\n'; // 3
+  v1.push_back(7);  // {5, 5, 5, 7};
 
-  // 전체를 특정값으로 초기화 하는 방법
-int a[21];
-int b[21][21];
+  vector<int> v2(2);  // {0, 0};
+  v2.insert(v2.begin() + 1, 3); // {0, 3, 0};
+  
+  vector<int> v3 = {1, 2, 3, 4};  // {1, 2, 3, 4};
+  v3.erase(v3.begin() + 2); // {1, 2, 4};
 
-// 1.memset (비추천) cstring 헤더
-memset(a, 0, sizeof a);
-memset(b, 0, sizeof b);
-
-// 2.for (추천)
-for(int i = 0; i < 21; i++)
-	a[i] = 0;
-for(int i = 0; i < 21; i++)
-	for(int j = 0; j < 21; j++)
-    	b[i][j] = 0;
-        
-// 3.fill (추천)
-fill(a, a+21, 0);
-for(int i = 0; i < 21; i++)
-	fill(b[i], b[i]+21, 0);
-
+  vector<int> v4; // {}
+  v4 = v3;  // {1, 2, 4}
+  cout << v4[0] << v4[1] << v4[2] << '\n';  //124
+  v4.pop_back(); // {1, 2}
+  v4.clear(); // {}
 
 
   return 0;
