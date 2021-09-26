f8c4f47c247ff69f66b5b2c83a3f9cbec666f15c
tokaji <fpzk5656@gmail.com>
2021-09-27 03:32:30 +0900

    알파벳 세기. 그릇에 담아서 빠른 속도로 표현

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,19 +6,21 @@ using namespace std;
 
 // 바킹독 0x3강
 
+int bowl[26];
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     string S;
     cin >> S;
-    for(char i = 'a'; i <= 'z'; i++)
+
+    for(auto c:S)
+    bowl[c-'a']++;
+
+    for(int i = 0; i < 26; i++)
     {
-        int count = 0;
-        for(int j = 0; j < S.size(); j++)
-        {
-            if(i == S[j])
-                count++;
-        }
-        cout << count << ' ';
+        cout << bowl[i] << ' ';
     }
+
     return 0;
 }
