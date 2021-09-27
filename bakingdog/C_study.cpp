8ee085118bc71dcae74f0ee84949abc45b07072e
tokaji <fpzk5656@gmail.com>
2021-09-28 01:12:27 +0900

    코딩테스트용 야매 연결리스트

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -4,23 +4,21 @@ using namespace std;
 #include <algorithm>
 #include <cstring>
 
-// 바킹독 0x3강
+// 바킹독 0x4강
 
-int bowl[26];
+// 야매 연결리스트
+const int MX = 1000005;
+int dat[MX], pre[MX], nxt[MX];
+int unused = 1;
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string S;
-    cin >> S;
 
-    for(auto c:S)
-    bowl[c-'a']++;
-
-    for(int i = 0; i < 26; i++)
-    {
-        cout << bowl[i] << ' ';
-    }
-
+    //fill(시작위치, 끝위치, 초기화할 숫자)
+    fill(pre, pre + MX, -1);
+    fill(nxt, nxt + MX, -1);
+    
     return 0;
 }
