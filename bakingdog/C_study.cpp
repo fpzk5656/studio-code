0f1f9e060ad894438f58806b14cf707238cad8f8
tokaji <fpzk5656@gmail.com>
2021-10-02 23:55:26 +0900

    백준 1406 문제: 야매 연결 리스트 문제로 업로드

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -4,25 +4,79 @@ using namespace std;
 #include <algorithm>
 #include <cstring>
 #include <list>
+#include <string>
 
 // 바킹독 0x4강
 
+const int MX = 1000005;
+char dat[MX];
+int pre[MX];
+int nxt[MX];
+int unused = 1;
+
+void insert(int addr, int num)
+{
+
+}
+
+void erase(int addr)
+{
+
+}
+
+void traversal()
+{
+
+}
+
+// 백준 1406 : 에디터 문제
+// 야매 연결리스트로 해결하기 문제
 int main()
 {
-    list<int> L = {1,2}; // 1 2
-    list<int>::iterator t = L.begin(); // t는 1을 가리키는 중
-    L.push_front(10); // 10 1 2
-    cout << *t << '\n'; // t가 가리키는 값 = 1을 출력
-    L.push_back(5); // 10 1 2 5
-    L.insert(t, 6); // t가 가리키는 곳 앞에 6을 삽입, 10 6 1 2 5
-    t++;    // t를 1칸 앞으로 전진, 현재 t가 가리키는 값은 2
-    t = L.erase(t); // t가 가리키는 값을 제거, 그 다음 원소인 5의 위치를 반환
-                    // 10 6 1 5, t가 가리키는 값은 5
-    cout << *t << '\n';  // 5
-    for(auto i : L) cout << i << ' ';   //iterator가 치기 싫다면, auto t = L.begin();를 해왔어도 괜찮다 함
-    cout << '\n';
-    for(list<int>::iterator it = L.begin(); it != L.end(); it++)    //C++11 미만이라면, 이렇게 돌리는 수 밖에..
-        cout << *it << ' ';
-
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
+    string init;
+    cin >> init;
+    int cursor = 0;
+    for(auto c : init)
+    {
+        insert(cursor, c);
+        cursor++;
+    }
+    int q;
+    cin >> q;
+    while(q--)
+    {
+        char op;
+        cin >> op;
+        if(op == 'P')
+        {
+            char add;
+            cin >> add;
+            insert(cursor, add);
+            cursor = nxt[cursor];
+        }
+        else if(op == 'L')
+        {
+            if(pre[cursor] != -1)
+                cursor = pre[cursor];
+        }
+        else if(op == 'D')
+        {
+            if(nxt[cursor] != -1)
+                cursor = nxt[cursor];
+        }
+        else 
+        {
+            if(pre[cursor] != -1)
+            {
+                erase(cursor);
+                cursor = pre[cursor];
+            }
+        }
+    }
+    traversal();
     return 0;
 }
