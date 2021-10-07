1ff1ebd5c0be221d12ffa2bb05f07cd9ae8c2c8f
tokaji <fpzk5656@gmail.com>
2021-10-07 12:27:49 +0900

    배열로 선형 큐 구현

--- bakingdog/basic-algo-lecture-master/basic-algo-lecture-master/0x06/queue_test.cpp
+++ bakingdog/basic-algo-lecture-master/basic-algo-lecture-master/0x06/queue_test.cpp
@@ -1,24 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 const int MX = 1000005;
 int dat[MX];
 int head = 0, tail = 0;
 
-void push(int x){
-
+void push(int x)
+{
+  dat[tail++] = x;
 }
 
-void pop(){
-
+void pop()
+{
+  head++;
 }
 
-int front(){
-
+int front()
+{
+  return dat[head];
 }
 
-int back(){
-
+int back()
+{
+  return dat[tail - 1];
 }
 
 void test(){
