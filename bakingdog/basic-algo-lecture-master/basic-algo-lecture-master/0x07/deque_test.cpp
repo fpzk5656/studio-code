175d0cc786ac502857b793becd168e7c6f4e62e7
tokaji <fpzk5656@gmail.com>
2021-10-08 18:07:44 +0900

    배열로 deque 덱 만들어보기

--- bakingdog/basic-algo-lecture-master/basic-algo-lecture-master/0x07/deque_test.cpp
+++ bakingdog/basic-algo-lecture-master/basic-algo-lecture-master/0x07/deque_test.cpp
@@ -1,32 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+// const int MX = 1000005;
+// int dat[2*MX+1];
+// int head = MX, tail = MX;
 const int MX = 1000005;
-int dat[2*MX+1];
+int dat[2*MX + 1];
 int head = MX, tail = MX;
 
-void push_front(int x){
-  
+void push_front(int x)
+{
+  dat[--head] = x; 
 }
 
-void push_back(int x){
-  
+void push_back(int x)
+{
+  dat[tail++] = x;
 }
 
-void pop_front(){
-  
+void pop_front()
+{
+  head++;
 }
 
-void pop_back(){
-  
+void pop_back()
+{
+  tail--;
 }
 
-int front(){
-  
+int front()
+{
+  return dat[head];  
 }
 
-int back(){
-  
+int back()
+{
+  return dat[tail - 1];  
 }
 
 void test(){
