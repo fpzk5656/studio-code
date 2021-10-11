8606572022fcce3daf791e61de8f546de23ff926
tokaji <fpzk5656@gmail.com>
2021-10-11 10:23:47 +0900

    백준 2309번 일곱난장이 찾기 문제

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,22 +6,38 @@ using namespace std;
 #include <list>
 #include <string>
 
-int A[3];
+// 난장이 9명 중에 일곱 난장이 찾기
 
-// sort 사용해보기
+int mx[101];
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    for(int i = 0; i < 3; i++)
+    int sum = 0;
+    for(int i = 0; i < 9; i++)
     {
-        cin >> A[i];
+        int k = 0;
+        cin >> k;
+        sum += k;
+        
+        mx[k] = 1;
     }
-    sort(A, A+3);
     
-    for(int i = 0; i < 3; i++)
+    sum -= 100;
+    
+    for(int i = 1; i < 101; i++)
     {
-        cout << A[i] <<' ';
+        if(mx[i] == 1 && mx[sum - i] == 1)
+        {
+            mx[i] = 0;
+            mx[sum - i] = 0;
+            break;
+        }
+    }
+    for(int i = 1; i < 101; i++)
+    {
+        if(mx[i] == 1)cout << i << '\n';
     }
     
     return 0;
