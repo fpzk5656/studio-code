4aeeaaa5ead1f9764278a80fcf3fbd80ca6ef1f7
tokaji <fpzk5656@gmail.com>
2021-10-12 11:41:52 +0900

    일곱 난쟁이 문제 답안 풀이 분석해봄. 근데 이건 초보자의 경우를 가정해서인지 복잡함. 이것보다 내가 만든게 더 간단명료한듯.

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -7,38 +7,38 @@ using namespace std;
 #include <string>
 
 // 난장이 9명 중에 일곱 난장이 찾기
+// (바킹독의 풀이 연구)
 
-int mx[101];
+int num[9], result[7];
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int sum = 0;
-    for(int i = 0; i < 9; i++)
-    {
-        int k = 0;
-        cin >> k;
-        sum += k;
-        
-        mx[k] = 1;
-    }
     
-    sum -= 100;
-    
-    for(int i = 1; i < 101; i++)
+    for(int i = 0; i < 9; i++) cin >> num[i];
+
+    // 9명 중에 2명을 뺀 조합 고려
+    for(int a = 0; a < 8; a++)
     {
-        if(mx[i] == 1 && mx[sum - i] == 1)
+        int total = 0;
+
+        for(int b = 0; b < 9; b++)
         {
-            mx[i] = 0;
-            mx[sum - i] = 0;
-            break;
+            total = 0;  // 돌릴 때마다 0으로 초기화
+            // 나머지 7명 저장
+            for(int c = 0, i = 0; c < 9; c++) if(c != a && c != b) result[i++] = num[c]; // c가 a랑 b와 같지 않을 때만 result에 저장
+            for(int i = 0; i < 7; i++)total += result[i];  // result 원소들 값을 합한다
+
+            // 7명 키의 합이 100인 경우
+            if(total == 100) break;
         }
+        if(total == 100) break;
     }
-    for(int i = 1; i < 101; i++)
-    {
-        if(mx[i] == 1)cout << i << '\n';
-    }
-    
+
+    // 정렬 후 출력
+    sort(result, result + 7);
+    for(int i = 0; i < 7; i++) cout << result[i] << '\n';
+
     return 0;
 }
