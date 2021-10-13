cda445c1303a3267866cb7ed15810f0e224cf703
tokaji <fpzk5656@gmail.com>
2021-10-13 16:25:29 +0900

    백준 1267 핸드폰 요금 문제

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -11,38 +11,43 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    long long A = 0;
-    long long B = 0;
-    long long C = 0;
-    cin >> A;
-    cin >> B;
-    if(A > B)swap(A,B);
-    if(A == B || (A - B == 0))
+    int N = 0;
+    cin >> N;
+    
+    int y = 0;
+
+    int m = 0;
+    //Y 30초10원 M 60초 15원
+    for(int i = 0; i < N; i++)
     {
+        int k = 0;
+        cin >> k;
         
+        int thirty = 0;
+        int minute = 0;
+        
+        thirty += k / 30;
+        if(k != 0 && (k % 30) < 30)thirty++;
+            
+        minute += k / 60;
+        if(k != 0 && (k % 60) < 60)minute++;
+        
+        y += thirty * 10;
+        m += minute * 15;
+    }
+    
+    if(m > y)
+    {
+        cout << "Y" << ' ' << y;
+    }
+    else if(m < y)
+    {
+        cout << "M" << ' ' << m;
     }
     else
     {
-        C = B - A - 1;
+        cout << "Y" << ' ' << "M" << ' ' << m; 
     }
-    
-    cout << C << '\n';
-    A += 1;
-    
-    long long s[C];
-    long long k = 0;
-    
-    while(A < B)
-    {
-        //cout << A << ' ';
-        s[k++] = A;
-        A++;
-    }
-    
-    for(int i = 0; i < C; i++)
-    {
-        cout << s[i] << ' ';
-    }
-        
+
     return 0;
 }
