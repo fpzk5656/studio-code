941e45f591bab4ce3baba44104d8eaf4bf64971d
tokaji <fpzk5656@gmail.com>
2021-10-14 19:39:16 +0900

    백준 10804 카드 역배치 문제 풀이

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,48 +6,49 @@ using namespace std;
 #include <list>
 #include <string>
 
-int main()
+int S[21];
+
+// 이게 안쓰일 수도 있음
+bool desc(int a, int b)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int N = 0;
-    cin >> N;
-    
-    int y = 0;
+    return a > b;
+}
 
-    int m = 0;
-    //Y 30초10원 M 60초 15원
-    for(int i = 0; i < N; i++)
+main()
+{
+    for(int i = 1; i < 21; i++)
     {
-        int k = 0;
-        cin >> k;
-        
-        int thirty = 0;
-        int minute = 0;
-        
-        thirty += k / 30;
-        if(k != 0 && (k % 30) < 30)thirty++;
-            
-        minute += k / 60;
-        if(k != 0 && (k % 60) < 60)minute++;
-        
-        y += thirty * 10;
-        m += minute * 15;
+        S[i] = i;
     }
     
-    if(m > y)
+    int N = 10;
+    
+    while(N--)
     {
-        cout << "Y" << ' ' << y;
-    }
-    else if(m < y)
-    {
-        cout << "M" << ' ' << m;
-    }
-    else
-    {
-        cout << "Y" << ' ' << "M" << ' ' << m; 
-    }
+        ios::sync_with_stdio(0);
+        cin.tie(0);
 
+        int k[2];
+        cin >> k[0];
+        cin >> k[1];
+        const int mx = (k[1]+1) - k[0];
+        int L[mx];
+        int count = mx -1;
+        for(int i = k[0]; i < k[1]+1; i++)
+        {
+            L[count--] = S[i];
+        }
+        int t = 0;
+        for(int i = k[0]; i < k[1]+1; i++)
+        {
+            S[i] = L[t++];
+        }
+    }
+    
+    for(int i = 1; i < 21; i++)
+    {
+        cout << S[i] << ' ';
+    }
+    
     return 0;
 }
