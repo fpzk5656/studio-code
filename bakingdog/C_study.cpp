d7ca4216700f3185c7c4a8c928e4451a77f09ca5
tokaji <fpzk5656@gmail.com>
2021-10-15 11:41:03 +0900

    백준 15552번 빠른 A+B 문제 풀이

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,49 +6,20 @@ using namespace std;
 #include <list>
 #include <string>
 
-int S[21];
-
-// 이게 안쓰일 수도 있음
-bool desc(int a, int b)
+int main()
 {
-    return a > b;
-}
-
-main()
-{
-    for(int i = 1; i < 21; i++)
-    {
-        S[i] = i;
-    }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
-    int N = 10;
-    
+    int N = 0;
+    cin >> N;
     while(N--)
     {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-
-        int k[2];
-        cin >> k[0];
-        cin >> k[1];
-        const int mx = (k[1]+1) - k[0];
-        int L[mx];
-        int count = mx -1;
-        for(int i = k[0]; i < k[1]+1; i++)
-        {
-            L[count--] = S[i];
-        }
-        int t = 0;
-        for(int i = k[0]; i < k[1]+1; i++)
-        {
-            S[i] = L[t++];
-        }
+        int A = 0;
+        int B = 0;
+        cin >> A;
+        cin >> B;
+        cout << A+B << '\n';
     }
-    
-    for(int i = 1; i < 21; i++)
-    {
-        cout << S[i] << ' ';
-    }
-    
     return 0;
 }
