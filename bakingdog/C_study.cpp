d4b5b670eba4a7272ab44f03e78a7d34627caee8
tokaji <fpzk5656@gmail.com>
2021-10-21 17:58:12 +0900

    백준 2577 0~9 숫자 개수 구하기

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,46 +6,30 @@ using namespace std;
 #include <list>
 #include <string>
 
+int k[10];
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int N = 0;
-    cin >> N;
-    
-    for(int i = 1; i < N+1; i++)
+    int A,B,C = 0;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    int N = A * B * C;
+    while(true)
     {
-        for(int k = 0; k < i; k++)
-        {
-            cout << "*";
-        }
-        for(int k = (2*N) - (i * 2); k > 0; k--)
-        {
-            cout << " ";
-        }
-        for(int k = 0; k < i; k++)
-        {
-            cout << "*";
-        }
-        cout << '\n';
+        int a = N%10;
+        k[a]++;
+        if(N < 10)break;
+        N /= 10;
     }
     
-    for(int i  = N - 1; i >= 0; i--)
+    for(int i = 0; i < 10; i++)
     {
-        for(int k = 0; k < i; k++)
-        {
-            cout << "*";
-        }
-        for(int k = 0; k < (N*2) - (i*2); k++)
-        {
-            cout << " ";
-        }
-        for(int k = 0; k < i; k++)
-        {
-            cout << "*";
-        }
-        cout << '\n';
+        cout << k[i] << '\n';
     }
+    
     return 0;
 }
