4e7c8071d70ad0b20552c50d8ac5392b150c5bea
tokaji <fpzk5656@gmail.com>
2021-10-22 20:20:16 +0900

    1475 방 번호 문제

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,30 +6,37 @@ using namespace std;
 #include <list>
 #include <string>
 
-int k[10];
+int S[10];
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int N = 0;
+    cin >> N;
     
-    int A,B,C = 0;
-    cin >> A;
-    cin >> B;
-    cin >> C;
-    int N = A * B * C;
-    while(true)
+    // 만약 인덱스 마다 이미 채워져있다고 쳐
+    // 2가 3개, 3이 4개야. 그럼 셋트는 당연히 4개가 필요하지
+    while(N > 0)
     {
-        int a = N%10;
-        k[a]++;
-        if(N < 10)break;
+        S[N%10]++;
         N /= 10;
     }
-    
+    int num = 0;
+    int sixOrNine = ((S[6] + S[9]) / 2) + ((S[6] + S[9]) % 2);
     for(int i = 0; i < 10; i++)
     {
-        cout << k[i] << '\n';
+        if(i != 6 && i != 9)
+        {
+            if(S[i] > num)num = S[i];
+        }
     }
+    
+    if(num < sixOrNine)num = sixOrNine;
+    
+    cout << num;
+    // 6과 9는 개수 합산해서 2로 나누고,
+    // 나눈 숫자로 가장 큰 셋트수와 비교해서 결정한다
     
     return 0;
 }
