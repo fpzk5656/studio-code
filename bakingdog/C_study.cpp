eb605ba2dbef636e186a7b1a2c14103080a26f0f
tokaji <fpzk5656@gmail.com>
2021-10-23 21:59:17 +0900

    백준 3273번 문제

--- bakingdog/C_study.cpp
+++ bakingdog/C_study.cpp
@@ -6,37 +6,34 @@ using namespace std;
 #include <list>
 #include <string>
 
-int S[10];
-
+int s[2000001];
+int k[1000001];
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N = 0;
     cin >> N;
-    
-    // 만약 인덱스 마다 이미 채워져있다고 쳐
-    // 2가 3개, 3이 4개야. 그럼 셋트는 당연히 4개가 필요하지
-    while(N > 0)
+    //int k[N];
+    for(int i = 0; i < N; i++)
     {
-        S[N%10]++;
-        N /= 10;
+        int c = 0;
+        cin >> c;
+        k[i] = c;
+        s[c] = 1 + i;
     }
-    int num = 0;
-    int sixOrNine = ((S[6] + S[9]) / 2) + ((S[6] + S[9]) % 2);
-    for(int i = 0; i < 10; i++)
+    int x = 0;
+    cin >> x;
+    int cnt = 0;
+    for(int i = 0; i < N; i++)
     {
-        if(i != 6 && i != 9)
-        {
-            if(S[i] > num)num = S[i];
-        }
+        int t = x - k[i];
+        
+        //S[t]에 숫자가 있고, S[t]에 주어진 번호가 S[k[i]보다 더 크다면
+        if(s[t] > 0 && s[t] > s[k[i]])cnt++;
     }
     
-    if(num < sixOrNine)num = sixOrNine;
-    
-    cout << num;
-    // 6과 9는 개수 합산해서 2로 나누고,
-    // 나눈 숫자로 가장 큰 셋트수와 비교해서 결정한다
+    cout << cnt;
     
     return 0;
 }
